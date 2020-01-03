ae8830b9a5e1d21491bec433dada0e6d98071a6c
reber <reber0ask@protonmail.com>
2020-01-03 11:05:21 +0800

    add guess_sid()

--- modules/oracle.py
+++ modules/oracle.py
@@ -4,39 +4,53 @@
 @Author: reber
 @Mail: reber0ask@qq.com
 @Date: 2019-09-25 21:11:15
-@LastEditTime: 2020-01-02 12:53:02
+@LastEditTime: 2020-01-03 11:03:30
 '''
 
 import socket
 import cx_Oracle
 from concurrent.futures import ThreadPoolExecutor
+
+from config import dict_path
+from libs.utils import get_content
 from libs.brute import BruteBaseClass
 
 class OracleBruteForce(BruteBaseClass):
     """OracleBruteForce"""
+
+    def guess_sid(self,host,port):
+        """
+        ORA-01017 用户名或密码不对
+        ORA-12505 sid不正确
+        """
+
+        sid_filename = dict_path.joinpath("sid.txt")
+        sid_list = get_content(sid_filename)
+        for sid in sid_list:
+            dsn = cx_Oracle.makedsn(host=host, port=port, sid=sid)
+            try:
+                connection = cx_Oracle.connect("sys", "123", dsn, encoding="UTF-8")
+            except cx_Oracle.DatabaseError as e:
+                code = e.args[0].code
+                message = e.args[0].message
+                if code == 1017:
+                    return sid
 
     def worker(self,hpup):
         if self.flag:
             socket.setdefaulttimeout(self.timeout)
             host,port,user,pwd = hpup
 
-            oracle_tns1 = "{}/{}@{}:{}/xe".format(user,pwd,host,port)
-            oracle_tns2 = "{}/{}@{}:{}/orcl".format(user,pwd,host,port)
-            try:
-                conn = cx_Oracle.connect(oracle_tns1)
-            except cx_Oracle.DatabaseError as e:
+            sid = self.guess_sid(host,port)
+            if sid:
+                dsn = cx_Oracle.makedsn(host=host, port=port, sid=sid)
                 try:
-                    conn = cx_Oracle.connect(oracle_tns2)
+                    conn = cx_Oracle.connect(user, pwd, dsn, encoding="UTF-8", threaded = True)
                 except Exception as e:
                     hook_msg((False,host,port,user,pwd))
                 else:
                     hook_msg((True,host,port,user,pwd))
                     conn.close()
-            except Exception as e:
-                hook_msg((False,host,port,user,pwd))
-            else:
-                hook_msg((True,host,port,user,pwd))
-                conn.close()
 
 
 bruter = OracleBruteForce
