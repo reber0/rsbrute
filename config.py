ac4fe9945b65a706bbd02edb9da673a29aafca4f
reber <reber0ask@qq.com>
2020-01-06 10:25:37 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,19 +7,13 @@
 @LastEditTime: 2019-12-26 14:55:36
 '''
 
-
 import pathlib
-from libs.mylog import MyLog
-
 
 
 root_abspath = pathlib.Path(__file__).parent.resolve()  #绝对路径
-dict_path = root_abspath.joinpath("dict")
 module_path = root_abspath.joinpath("modules")
+dict_path = root_abspath.joinpath("dict")
 log_file_path = root_abspath.joinpath("log")
-
 
 # log_level = ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 log_level = "INFO"
-
-
