abdb32ff6392d575956d33bf133d8689d1dc33a2
reber <reber0ask@protonmail.com>
2020-05-10 03:29:20 +0800

    update mylog.py

--- libs/mylog.py
+++ libs/mylog.py
@@ -4,37 +4,41 @@
 @Author: reber
 @Mail: reber0ask@qq.com
 @Date: 2019-07-16 22:31:00
-@LastEditTime: 2020-01-16 09:36:12
+@LastEditTime : 2020-05-10 03:29:00
 '''
 
 import logging
 import colorlog
 
+
 class MyLog(object):
     """
     MyLog(loglevel, logger_name, logfile=None)
     """
+
     def __init__(self, loglevel, logger_name, logfile=None):
         # 创建一个 logger
         self.logger = logging.getLogger(logger_name)
         self.logger.setLevel(logging.DEBUG)
 
-        #设置日志输出等级，后面创建 Handler 然后 setLevel(logging.DEBUG) 也不能输出 WARNING 等级之下的日志
+        # 设置日志输出等级，后面创建 Handler 然后 setLevel(logging.DEBUG) 也不能输出 WARNING 等级之下的日志
         # self.logger.setLevel(logging.WARNING)
 
         # 创建 stream hander
         sh = self.__stream_hander(loglevel)
-        self.logger.addHandler(sh)
+        if not self.logger.handlers: # 如果 handler 没有时才添加，为了解决日志重复问题
+            self.logger.addHandler(sh)
 
         # 创建 file hander
         if logfile:
             fh = self.__file_hander(logfile)
-            self.logger.addHandler(fh)
+            if not self.logger.handlers: # 如果 handler 没有时才添加，为了解决日志重复问题
+                self.logger.addHandler(fh)
 
     def __file_hander(self, logfile):
         # 创建一个用于写入日志文件的 handler
         fh = logging.FileHandler(logfile)
-        fh.setLevel(logging.DEBUG) # 只要是写入文件的等级都为DEBUG，也可以设置为loglevel
+        fh.setLevel(logging.DEBUG)  # 只要是写入文件的等级都为DEBUG，也可以设置为loglevel
         # formatterf = logging.Formatter('%(asctime)s [%(levelname)s] [%(name)s] %(message)s')
         formatterf = logging.Formatter('%(asctime)s [%(name)s] %(message)s')
         fh.setFormatter(formatterf)
@@ -62,19 +66,19 @@ class MyLog(object):
         sh.setFormatter(formatter)
         return sh
 
-    def critical(self,msg):
+    def critical(self, msg):
         self.logger.critical(msg)
 
-    def error(self,msg):
+    def error(self, msg):
         self.logger.error(msg)
 
-    def warning(self,msg):
+    def warning(self, msg):
         self.logger.warning(msg)
 
-    def info(self,msg):
+    def info(self, msg):
         self.logger.info(msg)
 
-    def debug(self,msg):
+    def debug(self, msg):
         self.logger.debug(msg)
 
 
